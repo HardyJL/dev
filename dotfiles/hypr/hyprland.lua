d69b61fc3b5373ddc2024d609dda317f705f77ca
Johannes <johannes.hardenberg@hotmail.com>
2026-07-18 20:57:39 +0200

    dev commit

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -1,6 +1,6 @@
 local terminal = "ghostty"
 local fileManager = "yazi"
-local menu = "wofi --show drun -p \"Search\""
+local menu = 'wofi --show drun -p "Search"'
 local browser = "firefox"
 
 _G.terminal = terminal
@@ -9,20 +9,21 @@ _G.menu = menu
 _G.browser = browser
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd(browser)
-    hl.exec_cmd("nm-applet &")
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("swaync")
-    hl.exec_cmd("systemctl --user start hyprpolkitagent")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("batsignal")
-    hl.exec_cmd("swayosd-server")
-    hl.exec_cmd("sh -c \"sleep 1 && $HOME/dev/scripts/wall.sh\"")
+	hl.exec_cmd(browser)
+	hl.exec_cmd("nm-applet &")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("swaync")
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("batsignal")
+	hl.exec_cmd("swayosd-server")
+	hl.exec_cmd("waypaper --restore")
+	hl.exec_cmd('sh -c "sleep 1 && $HOME/dev/scripts/wall.sh"')
 end)
 
-hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme \"prefer-dark\"")
-hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme \"adw-gtk3-dark\"")
+hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"')
+hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"')
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
@@ -46,3 +47,4 @@ require("land.input")
 require("land.keybindings")
 require("land.monitors")
 require("workspaces")
+require("land.rules")
